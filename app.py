46045b539ef2fa6e902345cc53fefaa94fd6a04b
rikysyaputra06 <rikysyaputra06@gmail.com>
2020-11-12 13:04:26 +0700

    Update app.py
    Mencoba memperbaiki Responsenya lagi
    
    Co-Authored-By: rikysyaputra06 <16067418+rikysyaputra06@users.noreply.github.com>

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ class ContohResource(Resource):
         umur = request.form["umur"]
         identitas["nama"] = nama
         identitas["umur"] = umur
-        response = {"msg" : "Success Bos QU"}
+        response = {"msg" : "Data Input Success"}
         return response
 
 # setup resourcenya
